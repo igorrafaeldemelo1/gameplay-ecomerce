51d93a72bf8917fd1b8ae95c6e04b5b388645171
igorrafaeldemelo1 <86748756+igorrafaeldemelo1@users.noreply.github.com>
2022-12-03 00:19:13 -0300

    segunda att

---
+++ js/javas.js
@@ -0,0 +1,279 @@
+var priceTotal = 0
+var shopCont = 0
+var listaitem = [
+    ['', 0],
+    ['Horizon', 235.99],
+    ['Final Fantasy XII', 145.99],
+    ['Bloodborne', 125.99],
+    ['No man´s sky', 110.99],
+    ['Batlefield V', 335.99],
+    ['Visage', 265.99],
+    ['The last oF Us', 277.99],
+    ['The last of Us II', 435.99],
+    ['Call od Duty', 135],
+    ['Ghostwire', 185.99],
+    ['Farcry 3', 135.99],
+    ['The Quarry', 195.99],
+    ['Returnal', 145.99],
+    ['Dishonred 2', 285.99],
+    ['kena', 215.99],
+    ['Death Stranding', 200.99],
+    ['Demons´s Souls', 288.99],
+    ['Jedi ', 205.99],
+    ['Assasin´s creed', 299.99],
+    ['Gal Fall ', 125.99]];
+
+function sold00() {
+    const close = document.querySelector('.close-shop');
+    const compra = document.querySelector('.shop');
+    compra.style.display = 'block'
+    close.style.display = 'block'
+}
+function close00() {
+    let close = document.querySelector('.shop')
+    close.style.display = 'none';
+}
+function sold01() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-01');
+    compra.style.display = 'block'
+}
+function close01() {
+    let close = document.querySelector('.shop-01')
+    close.style.display = 'none';
+}
+function sold02() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-02');
+    compra.style.display = 'block'
+}
+function close02() {
+    let close = document.querySelector('.shop-02')
+    close.style.display = 'none';
+}
+function sold03() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-03');
+    compra.style.display = 'block'
+}
+function close03() {
+    let close = document.querySelector('.shop-03')
+    close.style.display = 'none';
+}
+function sold04() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-04');
+    compra.style.display = 'block'
+}
+function close04() {
+    let close = document.querySelector('.shop-04')
+    close.style.display = 'none';
+}
+function sold05() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-05');
+    compra.style.display = 'block'
+}
+function close05() {
+    let close = document.querySelector('.shop-05')
+    close.style.display = 'none';
+}
+function sold06() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-06');
+    compra.style.display = 'block'
+}
+function close06() {
+    let close = document.querySelector('.shop-06')
+    close.style.display = 'none';
+}
+function sold07() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-07');
+    compra.style.display = 'block'
+}
+function close07() {
+    let close = document.querySelector('.shop-07')
+    close.style.display = 'none';
+}
+function sold08() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-08');
+    compra.style.display = 'block'
+}
+function close08() {
+    let close = document.querySelector('.shop-08')
+    close.style.display = 'none';
+}
+function sold09() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-09');
+    compra.style.display = 'block'
+}
+function close09() {
+    let close = document.querySelector('.shop-09')
+    close.style.display = 'none';
+}
+function sold10() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-10');
+    compra.style.display = 'block'
+}
+function close10() {
+    let close = document.querySelector('.shop-10')
+    close.style.display = 'none';
+} function sold11() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-11');
+    compra.style.display = 'block'
+}
+function close11() {
+    let close = document.querySelector('.shop-11')
+    close.style.display = 'none';
+}
+function sold12() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-12');
+    compra.style.display = 'block'
+}
+function close12() {
+    let close = document.querySelector('.shop-12')
+    close.style.display = 'none';
+}
+function sold13() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-13');
+    compra.style.display = 'block'
+}
+function close13() {
+    let close = document.querySelector('.shop-13')
+    close.style.display = 'none';
+}
+function sold14() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-14');
+    compra.style.display = 'block'
+}
+function close14() {
+    let close = document.querySelector('.shop-14')
+    close.style.display = 'none';
+}
+function sold15() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-15');
+    compra.style.display = 'block'
+}
+function close15() {
+    let close = document.querySelector('.shop-15')
+    close.style.display = 'none';
+}
+function sold16() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-16');
+    compra.style.display = 'block'
+}
+function close16() {
+    let close = document.querySelector('.shop-16')
+    close.style.display = 'none';
+}
+function sold17() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-17');
+    compra.style.display = 'block'
+}
+function close17() {
+    let close = document.querySelector('.shop-17')
+    close.style.display = 'none';
+}
+function sold18() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-18');
+    compra.style.display = 'block'
+}
+function close18() {
+    let close = document.querySelector('.shop-18')
+    close.style.display = 'none';
+}
+function sold19() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-19');
+    compra.style.display = 'block'
+}
+function close19() {
+    let close = document.querySelector('.shop-19')
+    close.style.display = 'none';
+}
+function sold20() {
+    const close = document.querySelector('.close-shop');
+    close.style.display = 'block'
+    const compra = document.querySelector('.shop-20');
+    compra.style.display = 'block'
+}
+function close20() {
+    let close = document.querySelector('.shop-20')
+    close.style.display = 'none';
+}
+
+
+
+
+function get01(a1) {
+    priceTotal = priceTotal + listaitem[a1][1]
+    shopCont = shopCont + 1
+    console.log(priceTotal)
+    iconAcont()
+    close00()
+    close01()
+    close02()
+    close03()
+    close04()
+    close05()
+    close06()
+    close07()
+    close08()
+    close09()
+    close10()
+    close11()
+    close12()
+    close13()
+    close14()
+    close15()
+    close16()
+    close17()
+    close18()
+    close19()
+    close20()
+
+
+}
+
+function iconAcont(){
+    document.getElementById("cont").innerHTML = shopCont
+    if(shopCont > 0){
+        const bg = document.querySelector('.cont')
+    bg.style.backgroundColor = 'red'
+    }else{
+        const bg = document.querySelector('.cont')
+    bg.style.backgroundColor = ''
+    }
+}
+
